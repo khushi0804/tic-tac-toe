ff9c770f50d951085cb8989273eb45ab66663c6c
khushi0804 <kg830574@gmail.com>
2024-07-04 02:05:58 +0530

    changes buttons and colors

--- src/Components/ScoreBoard.js
+++ src/Components/ScoreBoard.js
@@ -5,10 +5,10 @@ const ScoreBoard = ({ xScore, oScore, xPlaying, tie }) => {
   return (
     <div className="scoreboard">
       <span className={`x-score ${xPlaying === true ? "xScoreboard" : ""}`}>
-        X - {xScore}
+        (Red) X - {xScore}
       </span>
       <span className={`o-score ${xPlaying === false ? "oScoreboard" : ""}`}>
-        O - {oScore}
+        (Blue)O - {oScore}
       </span>
       <span className={`o-tie ${tie > 0 ? "oTie" : ""}`}>Tie - {tie}</span>
     </div>
